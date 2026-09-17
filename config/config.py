a8de03f39798cb1935c01b724af3bedc269ed9db
Ridhi <ridhisaini0773@gmail.com>
2026-09-17 19:31:56 +0530

    config.py

--- config/config.py
+++ config/config.py
@@ -7,7 +7,7 @@ DB_CONFIG = {
     "host": "localhost",
     "port": 3306,
     "user": "root",
-    "password": "ridhi",   # <-- change this
+    "password": "mysql password",   # <-- change this
     "database": "biomedical_waste_robot",
 }
 
